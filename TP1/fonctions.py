62a0ba476eb951c594038058e264ad3bf5021c5c
Raphaël Lesueur <raphlesueur@gmail.com>
2025-09-04 00:21:09 +0200

    puissance sans ** avec boucle for

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -3,5 +3,9 @@ def puissance(a, b):
         raise TypeError("Only integers are allowed")
     # 0^b avec b < 0 est indéfini -> 1/0
     if a == 0 and b < 0:
-        raise ZeroDivisionError("0 cannot be raised to a negative power")
-    return a ** b
+        raise TypeError("0 cannot be raised to a negative power")
+    var = 1
+    for _ in range(abs(b)):
+        var *= a
+    return var if b > 0 else 1 / var
+
