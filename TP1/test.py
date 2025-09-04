6a2c0eeacf202b98bf24c30d607c4269b0a5d1f2
Dino_Le_Dozo <raphlesueur@gmail.com>
2025-09-04 15:53:29 +0200

    correction de test.py

--- TP1/test.py
+++ TP1/test.py
@@ -12,11 +12,11 @@ def test_2():
     assert f.puissance(-1, -2) == 1
     assert f.puissance(-2, -1) == -0.5
 
-def test_zero_power_positive():
+def test_zero_positif():
     assert f.puissance(0, 1) == 0
     assert f.puissance(0, 5) == 0
 
-def test_zero_power_negative_raises():
+def test_zero_negatif():
     with pytest.raises(ZeroDivisionError):
         f.puissance(0, -1)
     with pytest.raises(ZeroDivisionError):
