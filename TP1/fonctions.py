231dd88d5a4d82fab017ca2e382a111a48eb4cd9
Dino_Le_Dozo <raphlesueur@gmail.com>
2025-09-04 16:47:01 +0200

    ajout de l'exeption dans

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -1,6 +1,11 @@
 def puissance(a, b):
     if not type(a) is int or not type(b) is int:
         raise TypeError("Only integers are allowed")
+	
+	if a == 0 and b < 0:
+		raise ZeroDivisionError("je peux faire de puissance si a == 0") 
+    
+
     var = 1
     for _ in range(abs(b)):
         var *= a
