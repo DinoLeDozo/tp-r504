bcd0c7912bcd3598a6c6fbfd30ecb7f03e0d06d4
Dino_Le_Dozo <raphlesueur@gmail.com>
2025-09-04 15:10:22 +0200

    revoir le fonction parce que la condiftion pour le négatif ne marche pas

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -1,9 +1,6 @@
 def puissance(a, b):
     if not type(a) is int or not type(b) is int:
         raise TypeError("Only integers are allowed")
-    # 0^b avec b < 0 est indéfini -> 1/0
-    if a == 0 and b < 0:
-        raise TypeError("0 cannot be raised to a negative power")
     var = 1
     for _ in range(abs(b)):
         var *= a
