ff667eb5c2e2047b51e9d0559a2f86eebb18f03a
Dino_Le_Dozo <raphlesueur@gmail.com>
2025-09-11 18:52:13 +0200

    on sauvegrade

--- TP4/run_app.sh
+++ TP4/run_app.sh
@@ -1,24 +1,7 @@
-CONTAINER_NAME="tp4-app"
-IMAGE_NAME="im-tp4"
-NETWORK_NAME="net-tp4"
-HOST_PORT=5000
-CONTAINER_PORT=5000
-
-if ! docker network ls | grep -q "$NETWORK_NAME"; then
-    echo "Création du réseau Docker : $NETWORK_NAME"
-    docker network create "$NETWORK_NAME"
-fi
-
-# Supprimer un conteneur existant avec le même nom, s'il existe
-if docker ps -a --format '{{.Names}}' | grep -q "^${CONTAINER_NAME}$"; then
-    echo "Suppression de l'ancien conteneur : $CONTAINER_NAME"
-    docker rm -f "$CONTAINER_NAME"
-fi
-
-# Lancer le conteneur
-echo "Lancement du conteneur $CONTAINER_NAME à partir de l'image $IMAGE_NAME"
+#!/bin/bash
 docker run -d \
-    --name "$CONTAINER_NAME" \
-    --network "$NETWORK_NAME" \
-    -p $HOST_PORT:$CONTAINER_PORT \
-    "$IMAGE_NAME"
+  --name tp4-app \
+  --network net-tp4 \
+  -p 5000:5000 \
+  im-tp4
+
